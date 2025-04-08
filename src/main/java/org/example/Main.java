b3b6cf641bb008116e7b88a4a7f6ccb296c21df2
Igor Work <keaerta@gmail.com>
2025-04-08 15:54:43 +0500

    add int

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        int asd = 1;
+        //sfsd
+
         System.out.println("Hello, World!");
     }
 }
